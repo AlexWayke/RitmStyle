c4aeaf48242e9679d6982b38af8ec8bb85bdee89
AlexWayke <yuhcha2@yandex.ru>
2022-06-15 23:45:03 +0400

    Add slider library
    Add slick slider

---
+++ script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.reviews').slick({
+        slidesToShow: 3,
+        adaptiveHeight: true,
+        infinite: false,
+        centerMode: true,
+        variableWidth:true,
+        dots: true,
+    });
+});
